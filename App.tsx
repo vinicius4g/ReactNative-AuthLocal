7ccc919acc0cf710ab3b5817b34aa9908d5f851c
Vinicius Martins <viniciusdias1701@hotmail.com>
2023-10-31 16:26:25 -0300

    finish first step

--- App.tsx
+++ App.tsx
@@ -1,11 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, Button, Alert, StyleSheet } from 'react-native';
+import * as LocalAuthentication from 'expo-local-authentication';
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  async function verifyAvailableAuthentication() {
+    const isCompatible = await LocalAuthentication.hasHardwareAsync();
+    console.log(isCompatible);
+
+    const types = await LocalAuthentication.supportedAuthenticationTypesAsync();
+    console.log(
+      types.map(type => LocalAuthentication.AuthenticationType[type]),
+    );
+  }
+
+  async function handleAuthentication() {
+    const isBiometricEnrolled = await LocalAuthentication.isEnrolledAsync();
+    console.log({ isBiometricEnrolled });
+
+    if (!isBiometricEnrolled)
+      return Alert.alert(
+        'Login',
+        'Nenhuma biometria encontrada, Por favor cadastre em seu dispositivo.',
+      );
+
+    const auth = await LocalAuthentication.authenticateAsync({
+      promptMessage: 'Login com biometria',
+      fallbackLabel: 'Biometria não reconhecida',
+    });
+
+    console.log(auth);
+    setIsAuthenticated(auth.success);
+  }
+
+  useEffect(() => {
+    verifyAvailableAuthentication();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Usuário conectado: {isAuthenticated ? 'Sim' : 'Não'} </Text>
+      <Button title="Entrar" onPress={handleAuthentication} />
     </View>
   );
 }
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.js', '.json'],
+        alias: {
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@assets': './src/assets',
+          '@hooks': './src/hooks',
+          '@utils': './src/utils',
+          '@styles': './src/styles',
+          '@constants': './src/constants',
+          '@routes': './src/routes',
+        },
+      },
+    ],
+  ],
 };
